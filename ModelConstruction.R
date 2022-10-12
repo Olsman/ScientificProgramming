bdc35461ca2d5451f30d28f0eaeae1ef0770ba36
Olsman <96538703+Olsman@users.noreply.github.com>
2022-10-12 19:33:50 +0200

    trying to update the ML model
    Made a RF model; very poor accuracy
    take a look at the important features
    also, try two RF models for each data type and then continue on the proximity average thingy

--- ModelConstruction.R
+++ ModelConstruction.R
@@ -7,21 +7,6 @@
 # go to your personal working directory - set working directory
 DIR <- setwd("/Users/rosanolsmanx/Documents/Maastricht University/Courses/MSB1015 Scientific Programming")
 
-# load data
-PPMicrobiomeData <- read.csv("FilteredMicrobiome.csv", header = TRUE, row.names = 1, sep = ",", check.names = FALSE)
-PPMetabolite <- read.csv("FilteredMetabolite.csv", header = TRUE, row.names = 1, sep = ",", check.names = FALSE)
-MetaData <- data.frame(read_excel("./Dataset/MetaTumourData.xlsx"), row.names = "Mouse.ID")
-
-# further pre-process data - microbiome
-transMicrobiome <- as.data.frame(asinh(PPMicrobiomeData))
-
-# further pre-process data - metabolite
-source("PQNfunction.R")
-pqnMetabolites <- pqn(as.matrix(PPMetabolite))
-transMetabolites <- log2(pqnMetabolites)
-scaleMetabolites <- as.data.frame((transMetabolites - rowMeans(transMetabolites))/(apply(transMetabolites,1,sd)))
-
-
 # Packages
 # Required CRAN packages:
 CRANpackages <- c("tidyverse", "readxl", "devtools", "ggplot2", "dplyr", "caret")
@@ -44,6 +29,106 @@ for (pkg in BiocPackages) {
     BiocManager::install(pkg, ask = FALSE)
   require(as.character(pkg), character.only = TRUE)
 }
+
+# Loading data
+df_micro <- read.csv("FilteredMicrobiome.csv", header = TRUE, row.names = 1, sep = ",", check.names = FALSE)  # Microbiome rows:columns -> taxa:samples
+df_meta <- read.csv("FilteredMetabolite.csv", header = TRUE, row.names = 1, sep = ",", check.names = FALSE) # Metabolomics rows:columns -> metabolites:samples
+metadata <- data.frame(read_excel("./Dataset/MetaTumourData.xlsx"), row.names = "Mouse.ID")
+
+# further pre-process data - microbiome
+df_micro <- as.data.frame(asinh(df_micro))
+
+# further pre-process data - metabolite
+source("PQNfunction.R")
+df_meta <- pqn(as.matrix(df_meta))
+df_meta <- log2(df_meta)
+df_meta <- as.data.frame((df_meta - rowMeans(df_meta))/(apply(df_meta,1,sd)))
+
+remove <- setdiff(colnames(df_meta), colnames(df_micro))
+df_meta <- df_meta[, !(names(df_meta) %in% remove)]
+
+remove <- setdiff(colnames(df_micro), colnames(df_meta))
+df_micro <- df_micro[, !(names(df_micro) %in% remove)]
+
+df_com_f <- rbind(df_micro, df_meta)
+
+install.packages("rpart.plot")
+library(rpart.plot)
+
+str(t(df_com_f))
+summary(df_com_f)
+
+data <- as.data.frame(t(df_com_f))
+metcat <- metadata %>%
+  filter(rownames(metadata) %in% rownames(data))
+metcat$TumorB <- as.factor(ifelse(metcat$Tumors > 0, "1", "0"))
+data$Tumor <- metcat$TumorB
+# data$ID <- rownames(metcat)
+
+set.seed(667)
+train <- createDataPartition(data[,"Tumor"],
+                             p = 0.8,
+                             list = FALSE)
+
+data.trn <- data[train,]
+data.tst <- data[-train,]
+
+ctrl <- trainControl(method = "cv",
+                     number = 10)
+
+fit.cv <- train(Tumor ~ .,
+                data = data.trn,
+                methods = "rf",
+                trControl = ctrl,
+                tuneLength = 50) # only 44?
+
+
+print(fit.cv)
+plot(fit.cv)
+fit.cv$results
+
+pred <- predict(fit.cv, data.tst)
+confusionMatrix(table(data.tst[,"Tumor"], pred))
+
+VarImp <- varImp(fit.cv)
+VarImp <- VarImp$importance
+
+ggplot(data = metcat, aes(x = Category, fill = Sex)) +
+  geom_bar(position = position_dodge()) +
+  theme_classic() +
+  labs(title = "Gender per Condition", x = "Condition", y = "Count") +
+  scale_fill_manual(values=c("#F8766D", "#00BFC4")) + 
+  theme(plot.title = element_text(hjust = 0.5),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black"))
+
+VarImp <- arrange(VarImp, Overall)
+top10 <- tail(VarImp, n = 20)
+###########
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #concatenate
